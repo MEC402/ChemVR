061484d46ebf542934c7c21edf7502cdd6c7611e
Skarlet Williams <77312299+skarletwilliams@users.noreply.github.com>
2024-04-27 22:35:28 -0600

    Adds start menu

---
+++ Assets/Scripts/Start_Module.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Module : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject myGameObject;
+    public void Show()
+    {
+        myGameObject.SetActive(true);
+    }
+}
