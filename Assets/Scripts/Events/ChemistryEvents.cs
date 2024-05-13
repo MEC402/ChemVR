90b48d0c1957ce337348bb13f5b6bc881a34e6e4
Skarlet Williams <77312299+skarletwilliams@users.noreply.github.com>
2024-05-13 15:23:38 -0600

    Added Code, deleted old task code

---
+++ Assets/Scripts/Events/ChemistryEvents.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChemistryEvents
+{
+    // TODO: Write this
+
+    /* IDEAS
+     * Pour
+     * Mix
+     * Spill
+     * Dispose
+     * 
+     */
+}
---
+++ Assets/Scripts/Events/GameEventsManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventsManager : MonoBehaviour
+{
+    public static GameEventsManager instance { get; private set; }
+
+    public ChemistryEvents chemistryEvents;
+    public TaskEvents taskEvents;
+    public InteractableEvents interactableEvents;
+    public PlayerEvents playerEvents;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Game Events Manager in the scene.");
+        }
+        instance = this;
+
+        // initialize all events
+        chemistryEvents = new ChemistryEvents();
+        taskEvents = new TaskEvents();
+        interactableEvents = new InteractableEvents();
+        playerEvents = new PlayerEvents();
+    }
+}
