6a007ad10255a9f0b16d9a1c704f5bfd66baccdc
Skarlet Williams <77312299+skarletwilliams@users.noreply.github.com>
2024-09-02 14:27:53 -0600

    RED DOTS VISIBLE

---
+++ Assets/Scripts/HygieneParticleManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HygieneParticleManager : MonoBehaviour
+{
+    Renderer myRenderer;
+
+    void Start()
+    {
+        myRenderer = this.GetComponent<Renderer>();
+
+        GameEventsManager.instance.partEvents.onShowParticles += showMe;
+        GameEventsManager.instance.partEvents.onHideParticles += hideMe;
+        GameEventsManager.instance.partEvents.onDeleteParticles += deleteMe;
+    }
+
+    private void showMe()
+    {
+        myRenderer.enabled = true;
+    }
+
+    private void hideMe()
+    {
+        myRenderer.enabled = false;
+    }
+
+    private void deleteMe()
+    {
+        Destroy(this);
+    }
+}
--- Assets/Scripts/HygieneTouchReporter.cs
+++ Assets/Scripts/HygieneTouchReporter.cs
@@ -16,53 +16,94 @@ public class HygieneTouchReporter : MonoBehaviour
 
     SkinnedMeshRenderer smr;
 
-    private void OnValidate() {
-        if (capsule == null) {
+    //Pasted in
+   /*  bool wearingRightGlove;
+    bool wearingLeftGlove;
+    private void OnEnable()
+    {
+
+        GameEventsManager.instance.taskEvents.TaskStartApproved("Glove_Hygiene");
+        GameEventsManager.instance.miscEvents.SetHint(GameObject.Find("glovebox"));
+
+        wearingLeftGlove = GameObject.Find("left hand model").GetComponent<SkinnedMeshRenderer>().material.name.ToLower().Contains("blue");
+        wearingRightGlove = GameObject.Find("right hand model").GetComponent<SkinnedMeshRenderer>().material.name.ToLower().Contains("blue");
+        GameEventsManager.instance.miscEvents.onPutOnRightGlove += RightGloveOn;
+        GameEventsManager.instance.miscEvents.onPutOnLeftGlove += LeftGloveOn;
+        GameEventsManager.instance.miscEvents.onTakeOffRightGlove += RightGloveOff;
+        GameEventsManager.instance.miscEvents.onTakeOffLeftGlove += LeftGloveOff;
+    }
+    private void OnDisable()
+    {
+        GameEventsManager.instance.miscEvents.onPutOnRightGlove -= RightGloveOn;
+        GameEventsManager.instance.miscEvents.onPutOnLeftGlove -= LeftGloveOn;
+        GameEventsManager.instance.miscEvents.onTakeOffRightGlove -= RightGloveOff;
+        GameEventsManager.instance.miscEvents.onTakeOffLeftGlove -= LeftGloveOff;
+    }*/
+    //Pasted in
+
+    private void OnValidate()
+    {
+        if (capsule == null)
+        {
             capsule = GetComponent<CapsuleCollider>();
         }
-        if (rb == null) {
+        if (rb == null)
+        {
             rb = GetComponent<Rigidbody>();
         }
-        if (smr == null) {
+        if (smr == null)
+        {
             smr = GetComponent<SkinnedMeshRenderer>();
         }
-        if (hygieneManager == null) {
+        if (hygieneManager == null)
+        {
             hygieneManager = FindFirstObjectByType<HygieneManager>();
         }
     }
 
-    private void Awake() {
-        if (capsule == null) {
+    private void Awake()
+    {
+        if (capsule == null)
+        {
             Debug.LogError($"No CapsuleCollider component assigned for {this.name}.");
-        } else if (!capsule.isTrigger) {
+        }
+        else if (!capsule.isTrigger)
+        {
             Debug.LogWarning($"{this.name}'s CapsuleCollider must be set to IsTrigger for hygiene touch to work.");
         }
-        if (rb == null) {
+        if (rb == null)
+        {
             Debug.LogError($"No Rigidbody component assigned for {this.name}.");
-        } else if (!rb.isKinematic) {
+        }
+        else if (!rb.isKinematic)
+        {
             Debug.LogWarning($"{this.name}'s Rigidbody must be set to IsKinematic for hygiene touch to work.");
         }
-        if (gloveMaterial == null) {
+        if (gloveMaterial == null)
+        {
             Debug.LogWarning($"No glove material set on object {this.name}. Please specify the glove material for proper glove on/off detection.");
         }
-        if (hygieneManager == null) {
+        if (hygieneManager == null)
+        {
             Debug.LogError($"No HygieneManager assigned in HygieneTouchReporter on {this.name}.");
         }
     }
 
 
-    private void OnTriggerEnter(Collider other) {
-        if (!smr.material.name.ToLower().Contains("glove")) {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!smr.material.name.ToLower().Contains("blue"))
+        {
             // Only track objects that are touched when gloves are on. Otherwise, just ignore the collision.
             return;
         }
-        if (other.isTrigger) {
+        if (other.isTrigger)
+        {
             // Also ignore other trigger colliders.
             return;
         }
         Vector3 touchPoint = other.ClosestPoint(this.transform.parent.position);
-        touchPoint = other.transform.InverseTransformPoint(touchPoint);
+
         hygieneManager.AddPoint(touchPoint, other.gameObject);
-        
     }
 }
