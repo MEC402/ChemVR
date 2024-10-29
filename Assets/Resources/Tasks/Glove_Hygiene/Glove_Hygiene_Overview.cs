e4d3c4b468916b48fbabc7b066d9fdc0721891e4
Steve Cutchin <smcutchin@koldertech.com>
2024-10-28 22:05:57 -0700

    better task text

--- Assets/Resources/Tasks/Glove_Hygiene/Glove_Hygiene_Overview.cs
+++ Assets/Resources/Tasks/Glove_Hygiene/Glove_Hygiene_Overview.cs
@@ -47,7 +47,7 @@ public class Glove_Hygiene_Overview : MonoBehaviour
         this.gameObject.GetComponent<ToggleTextSimple>().enabled = true;
     }
 
-    string[] text = {"Before beginning the lab, find gloves, goggles, and a lab coat and put them on with (A/X).\n\nRemove gloves anytime by holding your hands over the trashcan near the office door and pressing (A/X).\n\nRemember to take them off when you aren't working on the expirement!",
+    string[] text = {"Before beginning the lab, find gloves, goggles, and a lab coat and put them on with (A/X).\n\nRemove gloves anytime by holding your hands over the trashcan near the office door and pressing (A/X).\n\nRemember to take them off when you step away from the experiment!",
                     //"For this lab, bring the following to a table in the red zone / C zone:\n\nBeaker\nErlenmeyer flask with pink chemical\nErlenmeyer flask with blue chemical", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
                     //"You will need these items as well:\n\nBurret\nFunnel", //disabled task step 2_5 (add to TaskInfoSO block and uncomment to undo)
                     "You're getting a call! It might be urgent.\n\nYour phone is in the office on your right, pick it up and press (A/X) to answer it.",
@@ -55,14 +55,14 @@ public class Glove_Hygiene_Overview : MonoBehaviour
                     "You need to set up a burette.\n\nAttach a funnel and a burette to the holder at your table.",
                     "Next, fill the beaker one quarter way with the Erlenmeyer flask with the pink chemical.", //After 2 drops, interrupt with next one!
                     "It's time for a break.\n\nI think I saw some coffee in your office.\n\nPick it up and press (A/X) to take a drink.",
-                    "Breaks over!\n\nFinish filling the beaker the beaker one quarter way with the Erlenmeyer flask with the pink chemical.",
+                    "Breaks over!\n\nFinish filling the beaker one quarter way from the Erlenmeyer flask with the pink chemical.",
                     "Now fill the burette halfway from the Erlenmeyer flask with the blue chemical.\n\nTip: make sure the burette turner is closed first!",
-                    "Titrate from the burette into the beaker until you see a change of color in the beaker, or until you run out of blue chemical.\n\nWhen you're done, bring the beaker the the table with the notepad.",
+                    "Titrate from the burette into the beaker until you see a change of color in the beaker, or until you run out of blue chemical.\n\nWhen you're done, bring the beaker to the table with the notepad.",
                     "Uh Oh!\n\nThe printer is acting up again, and it's printing important data.\n\nSmack it once or twice with your hand to get it running right.", //Interrupt once you touch the turner
-                    "Return to the red zone and titrate until you see a change of color in the beaker, or until you run out of blue chemical.\n\nWhen you're done, bring the beaker the the table with the notepad.",
-                    "Record your findings using the pencil and data sheet on the tables near the office. Touch the pencil to the paper to do so.",
-                    "The titration experiment is now complete. Please go to the trash to remove your gloves.\n\nHold your hands over or in the trash and press A/X",
-                    "This completes the glove hygiene module. We will now display all of the chemical spills created throughout the experience.\n\n(Y Button) Hides Popup\n(B Button) Opens Menu"
+                    "Return to the red zone and titrate until you see a change of color in the beaker, or until you run out of blue chemical.\n\nWhen you're done, bring the beaker to the table with the notepad.",
+                    "Record your findings using the pencil and data sheet on the table near the office. Touch the pencil to the paper to do so.",
+                    "The titration experiment is now complete. Please go to the trash to remove your gloves.\n\nHold your hands over the trash and press A/X",
+                    "This completes the glove hygiene module. We will now display in red all of the things you touched with your gloves.\n\n(Y Button) Hides Popup\n(B Button) Opens Menu"
                     };
     void AdvanceGloTask(string context)
     {
