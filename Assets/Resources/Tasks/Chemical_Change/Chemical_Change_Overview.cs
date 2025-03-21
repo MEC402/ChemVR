2c34007e117071d05c211b8237068e12c517a949
Roswell_Winters <alessandra.l.wint@gmail.com>
2025-03-21 17:33:17 -0600

    project ready for friday upload. issue with no oculus menu being present

--- Assets/Resources/Tasks/Chemical_Change/Chemical_Change_Overview.cs
+++ Assets/Resources/Tasks/Chemical_Change/Chemical_Change_Overview.cs
@@ -45,30 +45,30 @@ public class Chemical_Change_Overview : MonoBehaviour
         this.gameObject.GetComponent<ToggleTextSimple>().enabled = true;
     }
 
-    string[] text = {"Welcome to the Physical & Chemical Change Lab!\nBefore we begin, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (B).",
-                    //"For this lab, bring the following to a table in the yellow zone:\n250mL beaker\n100mL graduated cylinder\nGlass stir rod\nStock Container of sugar (C6H12O6)\nWeigh boat and paper\n\n\nSkip with A", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
-                    //"Locate these tools that you will need in the lab:\nAnalytical Balance\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with A", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
-                    "Collect 50mL of DI water into graduated cylinder.\n\n\nSkip with A",
-                    "Configure the balance.\n\n\nSkip with A",
-                    "Add weigh paper to analytical balance and tare to 0.000g.\n\n\nSkip with A",
-                    "Weigh out 2g of sugar.\n\n\nSkip with A",
-                    "Fold up sugar in weigh paper and add to empty beaker.\n\n\nSkip with A",
-                    "Pour in 50mL water into beaker.\n\n\nSkip with A",
-                    "Stir with glass rod.\n\n\nSkip with A", //Solution is clear and transparent.
-                    "Is this a chemical change?\nYes or no?\n\n\nSkip with A", //TODO: Make this chooseable
-                    "Gather\n250mL beaker\nGlass stir rod\n2 volumetric pipettes\nStock Containers of:\nCrCl3\nNaOH\nH2O2.\n\n\nSkip with A",
-                    "Weigh out 2g of CrCl3 on weigh paper on analytical balance.\n\n\nSkip with A", //CrCl3 is Dark green crystalline dry flakes
-                    "Fold paper and dump into an empty beaker.\n\n\nSkip with A",
-                    "Set up 2 volumetric pipettes with a bulb on top.\n\n\nSkip with A",
-                    "Draw up 25mL of NaOH stock solution and place into beaker.\n\n\nSkip with A", //NaOH is clear, transparent liquid
-                    "Mix with a glass stir rod.\n\n\nSkip with A", //Color changes forming green solid clumps and the liquid is light green tinted.
-                    "Draw up 25mL of H2O2 stock solution in a new volumetric pipette and transfer into the beaker.\n\n\nSkip with A",
-                    "Mix with a glass stir rod.\n\n\nSkip with A", //Color changes from green to dark blue with fizzy bubbles on top.
-                    "Is this a chemical change?\nYes or no?\n\n\nSkip with A", 
-                    "Place the beaker of solution on a hot plate.\n\n\nSkip with A",
-                    "Turns the dial to ON, level 3 on the hot plate.\n\n\nSkip with A", //Liquid slowly boils and vapor comes off top of the beaker.
-                    "Is this a chemical change?\nYes or no?\n\n\nSkip with A", //Tell them if correct for each
-                    "This concludes the chemical change use module. Thank you for your time.\n\n(L Trigger) Hides Popup\n(R Trigger) Opens Menu"
+    string[] text = {"Welcome to the Physical & Chemical Change Lab!\nBefore we begin, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (X/A).\n\nPRESS B TO SKIP INSTRUCTIONS! ",
+                    //"For this lab, bring the following to a table in the yellow zone:\n250mL beaker\n100mL graduated cylinder\nGlass stir rod\nStock Container of sugar (C6H12O6)\nWeigh boat and paper\n\n\nSkip with B", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
+                    //"Locate these tools that you will need in the lab:\nAnalytical Balance\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with B", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
+                    "Collect 50mL of DI water into graduated cylinder.\n\n\nSkip with B",
+                    "Configure the balance.\n\n\nSkip with B",
+                    "Add weigh paper to analytical balance and tare to 0.000g.\n\n\nSkip with B",
+                    "Weigh out 2g of sugar.\n\n\nSkip with B",
+                    "Fold up sugar in weigh paper and add to empty beaker.\n\n\nSkip with B",
+                    "Pour in 50mL water into beaker.\n\n\nSkip with B",
+                    "Stir with glass rod.\n\n\nSkip with B", //Solution is clear and transparent.
+                    "Is this a chemical change?\nYes or no?\n\n\nSkip with B", //TODO: Make this chooseable
+                    "Gather\n250mL beaker\nGlass stir rod\n2 volumetric pipettes\nStock Containers of:\nCrCl3\nNaOH\nH2O2.\n\n\nSkip with B",
+                    "Weigh out 2g of CrCl3 on weigh paper on analytical balance.\n\n\nSkip with B", //CrCl3 is Dark green crystalline dry flakes
+                    "Fold paper and dump into an empty beaker.\n\n\nSkip with B",
+                    "Set up 2 volumetric pipettes with a bulb on top.\n\n\nSkip with B",
+                    "Draw up 25mL of NaOH stock solution and place into beaker.\n\n\nSkip with B", //NaOH is clear, transparent liquid
+                    "Mix with a glass stir rod.\n\n\nSkip with B", //Color changes forming green solid clumps and the liquid is light green tinted.
+                    "Draw up 25mL of H2O2 stock solution in a new volumetric pipette and transfer into the beaker.\n\n\nSkip with B",
+                    "Mix with a glass stir rod.\n\n\nSkip with B", //Color changes from green to dark blue with fizzy bubbles on top.
+                    "Is this a chemical change?\nYes or no?\n\n\nSkip with B", 
+                    "Place the beaker of solution on a hot plate.\n\n\nSkip with B",
+                    "Turns the dial to ON, level 3 on the hot plate.\n\n\nSkip with B", //Liquid slowly boils and vapor comes off top of the beaker.
+                    "Is this a chemical change?\nYes or no?\n\n\nSkip with B", //Tell them if correct for each
+                    "This concludes the chemical change use module. Thank you for your time.\n\n(Y) Hides Popup\n(Hamburger Button) Opens Menu"
             };
     void AdvanceCheTask(string context)
     {
--- Assets/Resources/Tasks/Glassware_Use/Glassware_Use_Overview.cs
+++ Assets/Resources/Tasks/Glassware_Use/Glassware_Use_Overview.cs
@@ -43,11 +43,11 @@ public class Glassware_Use_Overview : MonoBehaviour
         this.gameObject.GetComponent<ToggleTextSimple>().enabled = true;
     }
 
-    string[] text = {"Welcome to Glassware Identification!\nBefore we begin the lab, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (B).",
-                    //"For this lab, you will need to bring the required materials to a table in the blue zone, with tables labelled B:\nNote: Inspect all glassware for cracks or chips.\n\nStart with:\n3 250mL Volumetric Flasks.\n\n\nSkip with A", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
-                    //"Next bring:\n1 100mL Graduated Cylinder\n1 Glass Stir Rod\n1 10mL Volumetric Pipette\n to the table.\n\n\nSkip with A", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
-                    //"Finally you will need:\nSmall Weigh Boat With Paper\nCopper Sulfate\nPaper Towels\n\n\nSkip with A", //disabled task step 4 (add to TaskInfoSO block and uncomment to undo)
-                    //"Locate these tools that you will need in the lab:\nAnalytical Balance\nBalance Brush\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with A", //disabled task step 5 (add to TaskInfoSO block and uncomment to undo)
+    string[] text = {"Welcome to Glassware Identification!\nBefore we begin the lab, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (X/A). \n\nPRESS B TO SKIP INSTRUCTIONS! ",
+                    //"For this lab, you will need to bring the required materials to a table in the blue zone, with tables labelled B:\nNote: Inspect all glassware for cracks or chips.\n\nStart with:\n3 250mL Volumetric Flasks.\n\n\nSkip with B", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
+                    //"Next bring:\n1 100mL Graduated Cylinder\n1 Glass Stir Rod\n1 10mL Volumetric Pipette\n to the table.\n\n\nSkip with B", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
+                    //"Finally you will need:\nSmall Weigh Boat With Paper\nCopper Sulfate\nPaper Towels\n\n\nSkip with B", //disabled task step 4 (add to TaskInfoSO block and uncomment to undo)
+                    //"Locate these tools that you will need in the lab:\nAnalytical Balance\nBalance Brush\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with B", //disabled task step 5 (add to TaskInfoSO block and uncomment to undo)
                     "Inspect all glassware for cracks or chips.\n\n\nPick them up to inspect them.",
                     "Turn on the analytical balance, and allow for it to stabilize.\n\n\nTo do this, press the on button on the left side of the scale.",
                     "Place a weigh paper in a weigh boat, and then place the weigh boat on the scale.",
@@ -57,29 +57,29 @@ public class Glassware_Use_Overview : MonoBehaviour
                     "Remember to close the copper sulfate jar.",
                     "Pour the contents of the weigh paper into a 250ml flask.",
                     "Turn off the balance and clean it with a brush.",
-                    "Bring a graduated cylinder over to the DI water.\n\nFill it until the meniscus is at 50mL.\n\n\nSkip with A",
-                    "Bring the copper sulfate and DI water back to your hood, and then open the hood and lower it to a working height.\n\n\nSkip with A",
-                    "Set up a volumetric pipette with a bulb top.\n\nDraw up water to more than 50mL\nPush excess water out until 50mL\nTouch excess drip to side of the glass\nWipe the tip with folded paper towel.\n\n\nSkip with A",
-                    "Mix copper sulfate and water with the glass stir rod and write down any chemical changes in your notebook.\n\n\nSkip with A",
-                    "Line up 3 volumetric flasks.\n\nFill each flask with 15mL of the blue fluid using the volumetric pipette.\n\n\nSkip with A",
-                    "Fill flasks with 25mL, 35mL, 45mL water respectively and observe and record changes.\n\n\nSkip with A",
-                    "Time to clean up! Pre-rinse your glasses with tap water.\n\n\nSkip with A",
-                    "Wash your glassware with soapy water to remove any residues.\n\n\nSkip with A",
-                    "Scrub the inside of the glassware with a brush.\n\n\nSkip with A",
-                    "Rinse the glassware with the DI bottle.\n\n\nSkip with A",
-                    "Invert the glassware to allow excess water to drain.\n\n\nSkip with A",
-                    "Return materials to their original places, and close the glass doors.\n\nThis will protect them from dust and contaminants.\n\n\nSkip with A",
-                    "Go to the trash to remove your gloves, and then wash your hands in the sink with soap.\n\n\nSkip with A",
-                    "This concludes the glassware use module. Thank you for your time.\n\n(L Trigger) Hides Popup\n(R Trigger) Opens Menu"
+                    "Bring a graduated cylinder (Beaker) over to the DI water.\n\nFill it until the meniscus is at 50mL.\n\n\nSkip with B",
+                    "Bring the copper sulfate and DI water back to your hood, and then open the hood and lower it to a working height.\n\n\nSkip with B",
+                    "Set up a volumetric pipette with a bulb top.\n\nDraw up water to more than 50mL\nPush excess water out until 50mL\nTouch excess drip to side of the glass\nWipe the tip with folded paper towel.\n\n\nSkip with B",
+                    "Mix copper sulfate and water with the glass stir rod and write down any chemical changes in your notebook.\n\n\nSkip with B",
+                    "Line up 3 volumetric flasks.\n\nFill each flask with 15mL of the blue fluid using the volumetric pipette.\n\n\nSkip with B",
+                    "Fill flasks with 25mL, 35mL, 45mL water respectively and observe and record changes.\n\n\nSkip with B",
+                    "Time to clean up! Pre-rinse your glasses with tap water.\n\n\nSkip with B",
+                    "Wash your glassware with soapy water to remove any residues.\n\n\nSkip with B",
+                    "Scrub the inside of the glassware with a brush.\n\n\nSkip with B",
+                    "Rinse the glassware with the DI bottle.\n\n\nSkip with B",
+                    "Invert the glassware to allow excess water to drain.\n\n\nSkip with B",
+                    "Return materials to their original places, and close the glass doors.\n\nThis will protect them from dust and contaminants.\n\n\nSkip with B",
+                    "Go to the trash to remove your gloves, and then wash your hands in the sink with soap.\n\n\nSkip with B",
+                    "This concludes the glassware use module. Thank you for your time.\n\n(Y) Hides Popup\n(Hamburger Button) Opens Menu"
                 };
 
     // Updated to use WebGLText if isWebGL is true
     string[] webGLText = {
                     "Welcome!\nBefore we begin the lab, please hit Escape to pause the game and read the controls. After that, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (LeftClick/E).",
-                    //"For this lab, you will need to bring the required materials to a table in the blue zone, with tables labelled B:\nNote: Inspect all glassware for cracks or chips.\n\nStart with:\n3 250mL Volumetric Flasks.\n\n\nSkip with A", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
-                    //"Next bring:\n1 100mL Graduated Cylinder\n1 Glass Stir Rod\n1 10mL Volumetric Pipette\n to the table.\n\n\nSkip with A", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
-                    //"Finally you will need:\nSmall Weigh Boat With Paper\nCopper Sulfate\nPaper Towels\n\n\nSkip with A", //disabled task step 4 (add to TaskInfoSO block and uncomment to undo)
-                    //"Locate these tools that you will need in the lab:\nAnalytical Balance\nBalance Brush\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with A", //disabled task step 5 (add to TaskInfoSO block and uncomment to undo)
+                    //"For this lab, you will need to bring the required materials to a table in the blue zone, with tables labelled B:\nNote: Inspect all glassware for cracks or chips.\n\nStart with:\n3 250mL Volumetric Flasks.\n\n\nSkip with B", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
+                    //"Next bring:\n1 100mL Graduated Cylinder\n1 Glass Stir Rod\n1 10mL Volumetric Pipette\n to the table.\n\n\nSkip with B", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
+                    //"Finally you will need:\nSmall Weigh Boat With Paper\nCopper Sulfate\nPaper Towels\n\n\nSkip with B", //disabled task step 4 (add to TaskInfoSO block and uncomment to undo)
+                    //"Locate these tools that you will need in the lab:\nAnalytical Balance\nBalance Brush\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with B", //disabled task step 5 (add to TaskInfoSO block and uncomment to undo)
                     "Inspect all glassware for cracks or chips.\n\n\nPick them up to inspect them.",
                     "Turn on the analytical balance, and allow for it to stabilize.\n\n\nTo do this, press the on button on the left side of the scale.",
                     "Place a weigh paper in a weigh boat, and then place the weigh boat on the scale.",
@@ -89,19 +89,19 @@ public class Glassware_Use_Overview : MonoBehaviour
                     "Remember to close the copper sulfate jar.",
                     "Pour the contents of the weigh paper into a 250ml flask.",
                     "Turn off the balance and clean it with a brush.",
-                    "Bring a graduated cylinder over to the DI water.\n\nFill it until the meniscus is at 50mL.\n\n\nSkip with A",
-                    "Bring the copper sulfate and DI water back to your hood, and then open the hood and lower it to a working height.\n\n\nSkip with A",
-                    "Set up a volumetric pipette with a bulb top.\n\nDraw up water to more than 50mL\nPush excess water out until 50mL\nTouch excess drip to side of the glass\nWipe the tip with folded paper towel.\n\n\nSkip with A",
-                    "Mix copper sulfate and water with the glass stir rod and write down any chemical changes in your notebook.\n\n\nSkip with A",
-                    "Line up 3 volumetric flasks.\n\nFill each flask with 15mL of the blue fluid using the volumetric pipette.\n\n\nSkip with A",
-                    "Fill flasks with 25mL, 35mL, 45mL water respectively and observe and record changes.\n\n\nSkip with A",
-                    "Time to clean up! Pre-rinse your glasses with tap water.\n\n\nSkip with A",
-                    "Wash your glassware with soapy water to remove any residues.\n\n\nSkip with A",
-                    "Scrub the inside of the glassware with a brush.\n\n\nSkip with A",
-                    "Rinse the glassware with the DI bottle.\n\n\nSkip with A",
-                    "Invert the glassware to allow excess water to drain.\n\n\nSkip with A",
-                    "Return materials to their original places, and close the glass doors.\n\nThis will protect them from dust and contaminants.\n\n\nSkip with A",
-                    "Go to the trash to remove your gloves, and then wash your hands in the sink with soap.\n\n\nSkip with A",
+                    "Bring a graduated cylinder over to the DI water.\n\nFill it until the meniscus is at 50mL.\n\n\nSkip with B",
+                    "Bring the copper sulfate and DI water back to your hood, and then open the hood and lower it to a working height.\n\n\nSkip with B",
+                    "Set up a volumetric pipette with a bulb top.\n\nDraw up water to more than 50mL\nPush excess water out until 50mL\nTouch excess drip to side of the glass\nWipe the tip with folded paper towel.\n\n\nSkip with B",
+                    "Mix copper sulfate and water with the glass stir rod and write down any chemical changes in your notebook.\n\n\nSkip with B",
+                    "Line up 3 volumetric flasks.\n\nFill each flask with 15mL of the blue fluid using the volumetric pipette.\n\n\nSkip with B",
+                    "Fill flasks with 25mL, 35mL, 45mL water respectively and observe and record changes.\n\n\nSkip with B",
+                    "Time to clean up! Pre-rinse your glasses with tap water.\n\n\nSkip with B",
+                    "Wash your glassware with soapy water to remove any residues.\n\n\nSkip with B",
+                    "Scrub the inside of the glassware with a brush.\n\n\nSkip with B",
+                    "Rinse the glassware with the DI bottle.\n\n\nSkip with B",
+                    "Invert the glassware to allow excess water to drain.\n\n\nSkip with B",
+                    "Return materials to their original places, and close the glass doors.\n\nThis will protect them from dust and contaminants.\n\n\nSkip with B",
+                    "Go to the trash to remove your gloves, and then wash your hands in the sink with soap.\n\n\nSkip with B",
                     "This concludes the glassware use module. Thank you for your time.\n\n(L Trigger) Hides Popup\n(R Trigger) Opens Menu"
                 };
 
