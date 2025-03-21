de2fff6d98e9ae7ca9985163f493e072c88220f8
Roswell_Winters <alessandra.l.wint@gmail.com>
2025-03-20 20:27:30 -0600

    turned on Dev Ops for friday deadline

--- Assets/Scripts/Menu/DevOpsState.cs
+++ Assets/Scripts/Menu/DevOpsState.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class DevOpsState
 {
     // The devOps flag, default to false
-    private static bool devOps = false;
+    private static bool devOps = true; //for friday deadline. change back before student testing builds
 
     // Function to toggle the devOps boolean
     public static void ToggleDevOps()
