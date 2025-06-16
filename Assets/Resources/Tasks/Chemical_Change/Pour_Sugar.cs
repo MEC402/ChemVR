63a3f81c01129e7b7fb4edf66f70971b28c9d71e
Roswell_Winters <alessandra.l.wint@gmail.com>
2025-06-16 14:46:27 -0600

    you can pour sugar from boat into beaker. removed extra jar for solids (wasn't being used or configured correctly)

--- Assets/Resources/Tasks/Chemical_Change/Pour_Sugar.cs
+++ Assets/Resources/Tasks/Chemical_Change/Pour_Sugar.cs
@@ -13,18 +13,17 @@ public class Pour_Sugar : TaskStep
     }
     void OnEnable()
     {
-        GameEventsManager.instance.chemistryEvents.onPourIn += removeChem;
+        GameEventsManager.instance.chemistryEvents.onPourOut += removeChem; 
     }
     void OnDisable()
     {
-        GameEventsManager.instance.chemistryEvents.onPourIn -= removeChem;
+        GameEventsManager.instance.chemistryEvents.onPourOut -= removeChem;
     }
     private void removeChem(ChemContainer container, ChemFluid chemMix)
     {
-       // float twoThirdsFull = container.maxVolume / 1.5f; //if you divide by 1.5 you essentially multiply by 2/3
+        //float containerFill = container.maxVolume / 1;
         if (container.name.ToLower().Contains("weigh_boat_fluids_small")) //what is the object we are checking
         {
-            //Debug.Log("correctContainer");
             //NOTE: CHEM_MIX IS THE MIXTURE OF CHEMICALS ADDED TO THE BEAKER, NOT THE TOTAL CHEMICAL MIXTURE. USE CONTAINER.GETCONTENTS()
             string wholeContents = container.getContents();
             string[] sepWholeContents = wholeContents.Split('\n');
@@ -33,19 +32,15 @@ public class Pour_Sugar : TaskStep
                 //if the object being poured contains the "compoundName" or "chemicalName" 
                 if (s.Contains("SOLID_SUGAR") || s.Contains("SOLIDC6H12O6"))
                 {
-                   // Debug.Log("Contains sugar");
                     string strAmnt = s.Split(' ')[1];
                     float amt;
                     if (float.TryParse(strAmnt, out amt))
                     {
+                        //Debug.Log("boat amount: " + amt);
                         if (amt <= 0)
                         {
                             Debug.Log("poured complete contents into a beaker");
                             FinishTaskStep();
-                        }
-                        else
-                        {
-                            Debug.Log("not yet at 0   " + amt);
                         }
                     }
                 }
