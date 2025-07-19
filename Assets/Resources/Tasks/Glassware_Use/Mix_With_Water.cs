ad61690159c801e8492720b53616854486420e5f
Roswell_Winters <alessandra.l.wint@gmail.com>
2025-07-19 00:24:00 -0600

    stir, pour into beaker and set up pipette -- PLAYER BUG FOUND --

--- Assets/Resources/Tasks/Glassware_Use/Mix_With_Water.cs
+++ Assets/Resources/Tasks/Glassware_Use/Mix_With_Water.cs
@@ -5,27 +5,53 @@ using UnityEngine.InputSystem;
 
 public class Mix_With_Water : TaskStep
 {
+    private bool isWaterinBeaker = false;
     protected override void SetTaskStepState(string state)
     {
         throw new System.NotImplementedException();
     }
-    private void Update()
-    {
-        if (Keyboard.current.cKey.wasPressedThisFrame)
-        {
-            FinishTaskStep();
-        }
-    }
     void OnEnable()
     {
-        GameEventsManager.instance.inputEvents.onAButtonPressed += SkipTask;
+        GameEventsManager.instance.chemistryEvents.onPourIn += addChem;
+        GameEventsManager.instance.miscEvents.OnStirBeaker += StirEvent;
     }
     void OnDisable()
     {
-        GameEventsManager.instance.inputEvents.onAButtonPressed -= SkipTask;
+        GameEventsManager.instance.chemistryEvents.onPourIn -= addChem;
+        GameEventsManager.instance.miscEvents.OnStirBeaker -= StirEvent;
     }
-    private void SkipTask(InputAction.CallbackContext obj)
+
+    private void addChem(ChemContainer container, ChemFluid chemMix)
     {
-        FinishTaskStep();
+        float fiftyMlFull = container.maxVolume / 5; //max is 250mL
+        if (container.name.ToLower().Contains("BeakerUp250mL_largerText")) //what is the object we are checking
+        {
+            //NOTE: CHEM_MIX IS THE MIXTURE OF CHEMICALS ADDED TO THE BEAKER, NOT THE TOTAL CHEMICAL MIXTURE. USE CONTAINER.GETCONTENTS()
+            string wholeContents = container.getContents();
+            string[] sepWholeContents = wholeContents.Split('\n');
+            foreach (string s in sepWholeContents)
+            {
+                //if the object being poured contains the "compoundName" or "chemicalName" 
+                if (s.Contains("WATER") || s.Contains("H2O"))
+                {
+                    string strAmnt = s.Split(' ')[1];
+                    float amt;
+                    if (float.TryParse(strAmnt, out amt))
+                    {
+                        if (amt >= fiftyMlFull)
+                        {
+                            isWaterinBeaker = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void StirEvent()
+    {
+        if (isWaterinBeaker)
+        {
+            FinishTaskStep();
+        }
     }
 }
