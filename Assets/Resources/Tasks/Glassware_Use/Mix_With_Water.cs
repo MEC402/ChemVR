98586132636680f75a2fbbb877affb64e07c7fc6
Roswell_Winters <alessandra.l.wint@gmail.com>
2025-07-22 11:59:39 -0600

    added new task to collect water in beaker. currently task does not trigger event correctly but the order is correct and B button skips instructions as expected

--- Assets/Resources/Tasks/Glassware_Use/Mix_With_Water.cs
+++ Assets/Resources/Tasks/Glassware_Use/Mix_With_Water.cs
@@ -12,46 +12,10 @@ public class Mix_With_Water : TaskStep
     }
     void OnEnable()
     {
-        GameEventsManager.instance.chemistryEvents.onPourIn += addChem;
-        GameEventsManager.instance.miscEvents.OnStirBeaker += StirEvent;
+        GameEventsManager.instance.miscEvents.OnStirBeaker += FinishTaskStep; 
     }
     void OnDisable()
     {
-        GameEventsManager.instance.chemistryEvents.onPourIn -= addChem;
-        GameEventsManager.instance.miscEvents.OnStirBeaker -= StirEvent;
-    }
-
-    private void addChem(ChemContainer container, ChemFluid chemMix)
-    {
-        float fiftyMlFull = container.maxVolume / 5; //max is 250mL
-        if (container.name.ToLower().Contains("BeakerUp250mL_largerText")) //what is the object we are checking
-        {
-            //NOTE: CHEM_MIX IS THE MIXTURE OF CHEMICALS ADDED TO THE BEAKER, NOT THE TOTAL CHEMICAL MIXTURE. USE CONTAINER.GETCONTENTS()
-            string wholeContents = container.getContents();
-            string[] sepWholeContents = wholeContents.Split('\n');
-            foreach (string s in sepWholeContents)
-            {
-                //if the object being poured contains the "compoundName" or "chemicalName" 
-                if (s.Contains("WATER") || s.Contains("H2O"))
-                {
-                    string strAmnt = s.Split(' ')[1];
-                    float amt;
-                    if (float.TryParse(strAmnt, out amt))
-                    {
-                        if (amt >= fiftyMlFull)
-                        {
-                            isWaterinBeaker = true;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    void StirEvent()
-    {
-        if (isWaterinBeaker)
-        {
-            FinishTaskStep();
-        }
+        GameEventsManager.instance.miscEvents.OnStirBeaker -= FinishTaskStep; 
     }
 }
