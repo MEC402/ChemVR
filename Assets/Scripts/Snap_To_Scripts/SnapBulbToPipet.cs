76910cfa8b81bd1a7108f7145e6c637f93047e00
ZacharyBevan <142333986+zbbevan@users.noreply.github.com>
2026-01-05 15:57:46 -0700

    Made changes to Pipette bulbs
    Collision fixes made the bulbs not work as well. Added functionality so that the bulbs can now be attached to pipettes by pressing the F key in WebGL.

--- Assets/Scripts/Snap_To_Scripts/SnapBulbToPipet.cs
+++ Assets/Scripts/Snap_To_Scripts/SnapBulbToPipet.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 
 public class SnapBulbToPipet : MonoBehaviour
@@ -12,7 +13,8 @@ public class SnapBulbToPipet : MonoBehaviour
     private bool touching; //is this collider touching a pipet collieder?
     private bool snap; //is this bulb gameObject attached to a pipet?
     private bool isGrabbed; //is the bulb grabbed? (so pipet doesn't detach unless intentional)
-    private GameObject pipet; //the pipet gameobject
+    private bool isHeld = false; //is the bulb held via WebGL grab?
+    [SerializeField] private GameObject pipetteCollider; //the matching pipet object
 
     // ADDED FOR TESTING
     Vector3 OGbulbTranslation = new Vector3(0, 0, -0.001f);
@@ -46,6 +48,13 @@ public class SnapBulbToPipet : MonoBehaviour
         //Add listeners
         grabInteractable.selectEntered.AddListener(OnGrab);
         grabInteractable.selectExited.AddListener(OnRelease);
+
+        // WebGL listeners (if available)
+        if (GameEventsManager.instance != null && GameEventsManager.instance.webGLEvents != null)
+        {
+            GameEventsManager.instance.webGLEvents.OnObjectGrabbed += WebGLGrab;
+            GameEventsManager.instance.webGLEvents.OnObjectReleased += WebGLRelease;
+        }
     }
     private void SetPositionToPipet()
     {
@@ -53,27 +62,40 @@ public class SnapBulbToPipet : MonoBehaviour
         Quaternion additionalRotation = Quaternion.Euler(0, 0, 0);
 
         //Move the bulb to the burret
-        Quaternion newRotation = pipet.transform.rotation * additionalRotation;
-        this.transform.SetPositionAndRotation(pipet.transform.position, newRotation);
+        Quaternion newRotation = pipetteCollider.transform.rotation * additionalRotation;
+        this.transform.SetPositionAndRotation(pipetteCollider.transform.position, newRotation);
         this.transform.Translate(OGbulbTranslation);
     }
     private void LetGo()
     {
         snap = false;
-        pipet = null;
         myRb.useGravity = true;
     }
     private void OnDisable()
     {
         grabInteractable.selectEntered.RemoveListener(OnGrab);
         grabInteractable.selectExited.RemoveListener(OnRelease);
+
+        // WebGL listeners (if available)
+        if (GameEventsManager.instance != null && GameEventsManager.instance.webGLEvents != null)
+        {
+            GameEventsManager.instance.webGLEvents.OnObjectGrabbed -= WebGLGrab;
+            GameEventsManager.instance.webGLEvents.OnObjectReleased -= WebGLRelease;
+        }
+
+        // Make sure input events are cleaned up
+        if (GameEventsManager.instance != null && GameEventsManager.instance.inputEvents != null)
+        {
+            GameEventsManager.instance.inputEvents.onRTriggerPressed -= AttachBulb;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PipetteBulb") && !snap)
         {
-            pipet = other.gameObject;
+            // remember which pipette we're touching so we can snap to it
+            //pipetteCollider = other.gameObject;
             touching = true;
             GameEventsManager.instance.miscEvents.PippetConnectedFirst();
         }
@@ -98,6 +120,8 @@ public class SnapBulbToPipet : MonoBehaviour
     {
         isGrabbed = true;
         LetGo();
+        if (GameEventsManager.instance != null && GameEventsManager.instance.inputEvents != null)
+            GameEventsManager.instance.inputEvents.onRTriggerPressed += ForceSnapToPipette;
     }
     private void OnRelease(SelectExitEventArgs arg0)
     {
@@ -110,6 +134,78 @@ public class SnapBulbToPipet : MonoBehaviour
         else
         {
             LetGo();
+        }
+
+        // Unsubscribe input listeners to avoid leaks
+        if (GameEventsManager.instance != null && GameEventsManager.instance.inputEvents != null)
+        {
+            GameEventsManager.instance.inputEvents.onRTriggerPressed -= AttachBulb;
+        }
+    }
+
+    // WebGL handlers to mirror VR grab/release behavior (used by WebGL build handlers)
+    private void WebGLGrab(GameObject grabbedObject)
+    {
+        if (grabbedObject == gameObject)
+        {
+            isGrabbed = true;
+            isHeld = true;
+            LetGo();
+            if (GameEventsManager.instance != null && GameEventsManager.instance.inputEvents != null)
+                GameEventsManager.instance.inputEvents.onRTriggerPressed += AttachBulb;
+        }
+    }
+
+    private void WebGLRelease(GameObject releasedObject)
+    {
+        if (releasedObject == gameObject)
+        {
+            isGrabbed = false;
+            isHeld = false;
+            if (touching)
+            {
+                snap = true;
+                myRb.useGravity = false;
+            }
+            else
+            {
+                LetGo();
+            }
+
+            if (GameEventsManager.instance != null && GameEventsManager.instance.inputEvents != null)
+            {
+                GameEventsManager.instance.inputEvents.onRTriggerPressed -= AttachBulb;
+            }
+        }
+    }
+
+    private void ForceSnapToPipette(InputAction.CallbackContext context)
+    {
+        isGrabbed = false;
+        snap = true;
+        myRb.useGravity = false;
+    }
+
+    public void AttachBulb(InputAction.CallbackContext context)
+    {
+        if (isHeld)
+        {
+            GameObject player = GameObject.Find("FP Player");
+            if (player != null)
+            {
+                WebGLGrab webGrab = player.GetComponent<WebGLGrab>();
+                if (webGrab != null)
+                {
+                    webGrab.ForceReleaseObject();
+                    isHeld = false;
+                    isGrabbed = false;
+                    snap = true;
+                    myRb.useGravity = false;
+                    if (GameEventsManager.instance != null && GameEventsManager.instance.inputEvents != null)
+                        GameEventsManager.instance.inputEvents.onRTriggerPressed -= AttachBulb;
+                }
+            }
+
         }
     }
 }
