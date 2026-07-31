d28f116b2d1ebb4a3e3cdce07d38824ae8f9e04a
ZacharyBevan <142333986+zbbevan@users.noreply.github.com>
2026-07-31 15:52:38 -0600

    Adjust Sugar Jar but don't need to.
    It wasn't until I had been working on this for a while that I realized what we have is quite a bit different from the current script. The code is good but the order isn't, so we'll need to make some adjustments to the task steps and the order they are done in. We also need to make test tubes and a holder for them.

--- Assets/Resources/Tasks/Chemical_Change/Chemical_Change_Overview.cs
+++ Assets/Resources/Tasks/Chemical_Change/Chemical_Change_Overview.cs
@@ -45,18 +45,18 @@ public class Chemical_Change_Overview : MonoBehaviour
         this.gameObject.GetComponent<ToggleTextSimple>().enabled = true;
     }
 
-    string[] text = {"Welcome to the Physical & Chemical Change Lab!\nBefore we begin, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (X/A).\n\nPRESS B TO SKIP INSTRUCTIONS! ",
+    string[] text = {"Welcome to the Physical & Chemical Change Lab!\nBefore we begin, you need to put on your lab gear.\n\nYou should be wearing closed toed shoes and have your hair tied back.\n\nFind gloves, goggles, and a lab coat and put them on with (X/A).",
                     //"For this lab, bring the following to a table in the yellow zone:\n250mL beaker\n100mL graduated cylinder\nGlass stir rod\nStock Container of sugar (C6H12O6)\nWeigh boat and paper\n\n\nSkip with B", //disabled task step 2 (add to TaskInfoSO block and uncomment to undo)
                     //"Locate these tools that you will need in the lab:\nAnalytical Balance\nDI water\nWhen you are ready to continue, press (A).\n\n\nSkip with B", //disabled task step 3 (add to TaskInfoSO block and uncomment to undo)
-                    "Pour 50mL of DI water into graduated cylinder.\n\n\nSkip with B",
-                    "Configure the balance.\n\n\nSkip with B",
-                    "Add weigh paper to analytical balance and tare to 0.000g.\n\n\nSkip with B",
-                    "Weigh out 2g of sugar (White Jar).\n\n\nSkip with B",
-                    "Fold up sugar in weigh paper and add to empty beaker.\n\n\nSkip with B",
-                    "Pour in 50mL water from Graduated Cylinder into beaker.\n\n\nSkip with B",
-                    "Stir with glass rod.\n\n\nSkip with B", //Solution is clear and transparent.
+                    "Pour 50mL of DI water into graduated cylinder.",
+                    "Turn on the scale, and set the mode to grams.",
+                    "Add weigh paper to analytical balance and tare to 0.000g.",
+                    "Weigh out 2g of sugar (White Jar).",
+                    "Fold up sugar in weigh paper and add to empty beaker.",
+                    "Pour in 50mL water from Graduated Cylinder into beaker.",
+                    "Stir with glass rod.", //Solution is clear and transparent.
                     "Is this a chemical change?\nYes or no?\n\n\nSkip with B", //TODO: Make this chooseable
-                    "Gather\n250mL beaker\nGlass stir rod\n2 volumetric pipettes\nStock Containers of:\nCrCl3\nNaOH\nH2O2.\n\n\nSkip with B",
+                    "Gather\n250mL beaker\nGlass stir rod\n2 volumetric pipettes\nStock Containers of:\nCrCl3\nNaOH\nH2O2",
                     "Weigh out 2g of CrCl3 (Green Jar) on weigh paper on analytical balance.\n\n\nSkip with B", //CrCl3 is Dark green crystalline dry flakes
                     "Fold paper and dump into an empty beaker.\n\n\nSkip with B",
                     "Set up 2 volumetric pipettes with a bulb on top.\n\n\nSkip with B",
